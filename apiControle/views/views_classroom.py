0dca652d78863fe2baf9cfc1562d85aecae6f429
Vincenzo140 <vincenzo.amendola141@gmail.com>
2024-03-28 22:39:17 -0300

    feat: updating views

--- apiControle/views/views_classroom.py
+++ apiControle/views/views_classroom.py
@@ -1,21 +1,32 @@
 from django.views import View
+from django.shortcuts import render
 from django.http import JsonResponse
 from apiControle.models import Classroom, Schedule
 from apiControle.serializer import ClassroomSerializer
 from apiControle.repository import Repository
-
 class ClassroomListView(View):
     def get(self, request):
         repository = Repository('classrooms')
         classrooms = repository.getAll()
-        serializer = ClassroomSerializer(classrooms, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        context = {'classrooms': classrooms}
+        return render(request, 'index.html', context)
 
     def post(self, request):
+        repository = Repository('classrooms')
         serializer = ClassroomSerializer(data=request.POST)
         if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status=201)
+            dados_validos = serializer.save()
+            dados_dict = {
+                'id': dados_validos.id,
+                'name': dados_validos.name,
+                'capacity': dados_validos.capacity
+            }
+            # Realiza a inserção dos dados válidos no repositório
+            repository.insert(dados_dict)
+            # Após a inserção bem-sucedida, renderiza novamente a página com os novos dados
+            classrooms = repository.getAll()  # Atualiza a lista de salas de aula
+            context = {'classrooms': classrooms}
+            return render(request, 'index.html', context)
         return JsonResponse(serializer.errors, status=400)
 
 class ClassroomDetailsView(View):
@@ -24,7 +35,8 @@ class ClassroomDetailsView(View):
         classroom = repository.getById(id)
         if classroom:
             serializer = ClassroomSerializer(classroom)
-            return JsonResponse(serializer.data)
+            context = {'classroom': serializer.data}
+            return render(request, 'index.html', context)
         return JsonResponse({"error": "Classroom not found"}, status=404)
 
     def put(self, request, id):
