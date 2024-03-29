f5a2f06313b3fe4737360bd2e4e605c75c0e44fc
Vincenzo140 <vincenzo.amendola141@gmail.com>
2024-03-28 23:01:36 -0300

    refactor: chance urls

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from apiControle.views.views_classroom import ClassroomDetailsView,ClassroomListView
-from apiControle.views.views_reserve import ClassroomAvailableTimesView,ClassroomCancelReservationView, ClassroomReserveView
+from apiControle.views.views_classroom import ClassroomDetailsView, ClassroomListView
+from apiControle.views.views_reserve import ClassroomAvailableTimesView, ClassroomCancelReservationView, ClassroomReserveView
 
 urlpatterns = [
     path('api/salas/', ClassroomListView.as_view(), name='list_rooms'),
@@ -8,7 +8,7 @@ urlpatterns = [
     path('api/salas/criar/', ClassroomListView.as_view(), name='create_class'),
     path('api/salas/<int:id>/atualizar/', ClassroomDetailsView.as_view(), name='update_class'),
     path('api/salas/<int:id>/excluir/', ClassroomDetailsView.as_view(), name='delete_class'),
-    path('api/salas/<int:id>/horarios/', ClassroomAvailableTimesView.as_view() , name='list_available_times'),
+    path('api/salas/<int:id>/horarios/', ClassroomAvailableTimesView.as_view(), name='list_available_times'),
     path('api/salas/<int:id>/reservar/', ClassroomReserveView.as_view(), name='reserve_class'),
-    path('api/reservas/<int:id>/cancelar/', ClassroomCancelReservationView.as_view() , name='cancel_reservation'),
+    path('api/reservas/<int:id>/cancelar/', ClassroomCancelReservationView.as_view(), name='cancel_reservation'),
 ]
